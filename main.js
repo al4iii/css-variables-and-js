9d7ff0cd1124bb749e35abb0cbbfc61efe0701d4
Ali Da <al4icrux@gmail.com>
2021-03-11 10:21:17 +0300

    refactor: apply prettier;

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const inputs = document.querySelectorAll('.controls input');
+const inputs = document.querySelectorAll(".controls input");
 
-let handleUpdate = (e) => {
-  const suffix = e.target.dataset.sizing || '';
-  document.documentElement.style.setProperty(`--${e.target.name}`, e.target.value + suffix);
+const handleUpdate = (e) => {
+  const suffix = e.target.dataset.sizing || "";
+  document.documentElement.style.setProperty(`--${e.target.name}`,e.target.value + suffix );
 };
 
-document.body.addEventListener('change', handleUpdate);
-document.body.addEventListener('mousemove', handleUpdate);
+document.body.addEventListener("change", handleUpdate);
+document.body.addEventListener("mousemove", handleUpdate);
